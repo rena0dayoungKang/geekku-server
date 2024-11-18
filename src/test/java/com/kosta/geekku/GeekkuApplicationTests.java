69cfe2072882660415b2722590bcaf6ad720b63f
rena <dayoungrenakang@gmail.com>
2024-11-18 16:26:24 +0900

    create : .gradle, .properties 생성

--- src/test/java/com/kosta/geekku/GeekkuApplicationTests.java
+++ src/test/java/com/kosta/geekku/GeekkuApplicationTests.java
@@ -1,13 +1,13 @@
 package com.kosta.geekku;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GeekkuApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
