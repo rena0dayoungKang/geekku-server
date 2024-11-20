52285674fb314f607759a24d8f43f3aebf95c963
mel <codnjs9504@gmail.com>
2024-11-20 09:30:06 +0900

    develop first commit onestop controller detail and write

--- src/main/java/com/kosta/geekku/dto/UserDto.java
+++ src/main/java/com/kosta/geekku/dto/UserDto.java
@@ -1,5 +1,18 @@
 package com.kosta.geekku.dto;
 
-public class UserDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+	private String username;
+	private String name;
+	private String phone;
+	private String email;
+	private String nickname;
 }
