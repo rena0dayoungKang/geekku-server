e699ba5493463323f7f363a6806af71f1a239323
psw <pso0736@naver.com>
2024-11-22 17:32:39 +0900

    feat : 리스트 및 상세 등록하기

--- src/main/java/com/kosta/geekku/entity/InteriorReview.java
+++ src/main/java/com/kosta/geekku/entity/InteriorReview.java
@@ -1,7 +1,11 @@
 package com.kosta.geekku.entity;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -9,8 +13,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
+
+import com.kosta.geekku.dto.ReviewDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,15 +39,36 @@ public class InteriorReview {
 	@JoinColumn(name = "userId")
 	private User user;
 	// private UUID userId; //join column User - userId
+	
+	private String companyName;
+	private String type;
+	private String style;
+	private Integer size;
+	private String location;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "companyId")
-	private Company company;
+	@JoinColumn(name = "interiorNum")
+	private Interior interior;
 	// private UUID companyId; //join column Company -companyId
 
-	private Integer[] images;// TODO - 리뷰이미지가 여러개일 경우?!
-
+	@OneToMany(mappedBy = "interiorReview", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<InteriorReviewImage> imagesList = new ArrayList<>();
+	
 	private String content;
 	@CreationTimestamp
 	private Timestamp createdAt;
+	
+	public ReviewDto toDto() {
+		ReviewDto reviewDto = ReviewDto.builder().reviewNum(reviewNum).userId(user.getUserId())
+				.companyName(interior.getCompany().getCompanyName()).type(type)
+				.style(style).size(size).location(location).content(content).createdAt(createdAt)
+				.interiorNum(interior.getInteriorNum()).build();
+		
+		if(imagesList != null && imagesList.size() > 0) {
+			reviewDto.setImageNums(imagesList.stream().map(i -> i.getInteriorReviewImageNum() + "").collect(Collectors.joining(",")));
+		}
+		
+		return reviewDto;
+	}
+
 }
